124fe341bf9be622294b11740bdbefd44a9a1fb3
Remy349 <santoraga15@gmail.com>
2022-11-06 12:43:26 -0600

    HTML structure completed, just need to add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import { Card } from './components/Card'
+
+import supervisorIcon from './images/icon-supervisor.svg'
+import teamBuilder from './images/icon-team-builder.svg'
+import karmaIcon from './images/icon-karma.svg'
+import calculatorIcon from './images/icon-calculator.svg'
 
 function App() {
   return (
-    <main className='main'>
-      <h1>OK!!!</h1>
+    <main className='fourC'>
+      <div className='fourC__header'>
+        <h1 className='fourC__header-title'>
+          Reliable, efficient delivery
+          <br />
+          <span>Powered by Technology</span>
+        </h1>
+        <p className='fourC__desc'>
+          Our Artificial Intelligence powered tools use millions of project data
+          points to ensure that your project is successful
+        </p>
+      </div>
+      <section className='fourC__cards'>
+        <Card
+          title='Supervisor'
+          description='Monitors activity to identify project roadblocks'
+          icon={supervisorIcon}
+          altIcon='Supervisor Icon'
+        />
+        <Card
+          title='Team Builder'
+          description='Scans our talent network to create the optimal team for your project'
+          icon={teamBuilder}
+          altIcon='Team Builder Icon'
+        />
+        <Card
+          title='Karma'
+          description='Regularly evaluates our talent to ensure quality'
+          icon={karmaIcon}
+          altIcon='Karma Icon'
+        />
+        <Card
+          title='Calculator'
+          description='Uses data from past projects to provide better delivery estimates'
+          icon={calculatorIcon}
+          altIcon='Calculator Icon'
+        />
+      </section>
     </main>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Card = (props) => {
+  return (
+    <div className='fourC__cards-card'>
+      <h3 className='fourC__cards-card_title'>{props.title}</h3>
+      <p className='fourC__cards-card_desc'>{props.description}</p>
+      <div className='fourC__cards-card_contImg'>
+        <img
+          className='fourC__cards-card_img'
+          src={props.icon}
+          alt={props.altIcon}
+        />
+      </div>
+    </div>
+  )
+}
